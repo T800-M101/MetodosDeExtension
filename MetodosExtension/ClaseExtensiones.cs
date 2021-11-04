081584d4dc086c429084e0681205ae9ee3da7f11
Guillermo Moran Lihaut <guillermo.moran@office365.3pillarglobal.com>
2021-11-04 18:58:46 -0500

    Primer commit

---
+++ MetodosExtension/ClaseExtensiones.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosExtension
+{
+    // La clase donde se guardan los métodos de extención debe ser static
+    static class ClaseExtensiones
+    {
+        // El método de exyensión debe ser static y public. El parámetro this se refiere al tipo de dato que se va a extender
+        public static int Suma(this int i, int numero)
+        {
+            return i + numero;
+        }
+
+        public static int Multipla(this int i, int numero)
+        {
+            return i * numero;
+        }
+
+
+        public static int Divide(this int i, int numero)
+        {
+            int result = 0; ;
+            try
+            {
+                 result = i / numero;
+               
+                
+            }
+            catch (DivideByZeroException)
+            {
+
+                Console.WriteLine("No se puede dividir por cero");
+            }
+            return result;
+
+        }
+
+
+        public static int multiplica(int i, int numero)
+        {
+            return i * numero;
+        }
+    }
+}
---
+++ MetodosExtension/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MetodosExtension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 55;
+            Console.WriteLine("Ingresa un numero");
+            int numero2 = Int32.Parse(Console.ReadLine());
+            double resultado = numero.Suma(numero2);
+            Console.WriteLine($"Sumando {numero} y {numero2}...");
+            Console.WriteLine($"{numero} + {numero2} = {resultado}");
+
+
+
+            
+            Console.WriteLine("Ingresa un numero");
+            numero2 = Int32.Parse(Console.ReadLine());
+            resultado = numero.Divide(numero2);
+
+            if (numero2 == 0) return;
+            
+            
+            Console.WriteLine($"Dividiendo {numero} y {numero2}...");
+            Console.WriteLine($"{numero} / {numero2} = {resultado}");
+
+
+            Console.WriteLine("Ingresa un numero");
+            numero2 = Int32.Parse(Console.ReadLine());
+            resultado = numero.Multipla(numero2);
+            Console.WriteLine($"Multiplicando {numero} y {numero2}...");
+            Console.WriteLine($"{numero} * {numero2} = {resultado}");
+
+
+        }
+    }
+}
